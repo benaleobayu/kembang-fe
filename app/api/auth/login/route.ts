6dc8bd406b5f767cdbde267e04ddd62959168b49
benaleobayu <benaleobs@gmail.com>
2024-11-03 21:15:47 +0700

    ADD : API login

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,33 @@
+import {NextResponse, NextRequest} from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const {email,password} = await req.json();
+        const response = await fetch(`${process.env.AUTH_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, password}),
+        });
+
+        if (!response.ok) {
+            throw new Error('LOGIN FAILED');
+        }
+        const data : any = await response.json();
+
+        const token  = data.data.accessToken as string; // Adjust based on your actual response structure
+
+        const responseJson : any = NextResponse.json(data);
+
+        responseJson.cookies.set('accessToken', token)
+
+        return responseJson;
+    }catch (error) {
+        return NextResponse.json({
+            status: 500,
+            message: error.message,
+        });
+    }
+}
