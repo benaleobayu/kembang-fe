6b654f9e7e92b383e9ab63f596cc604c9f48ea1b
benaleobayu <benaleobs@gmail.com>
2024-11-03 21:13:46 +0700

    add layout cms to get sidebar

---
+++ app/cms/v1/layout.tsx
@@ -0,0 +1,20 @@
+// @flow
+import * as React from 'react';
+import __Navbar from "@/components/apps/globals/navbar/navbar";
+import {SideMenu} from "@/app/cms/v1/side-menu";
+
+export default function __Layout({children}: any) {
+    return (
+        <>
+            <__Navbar/>
+            <div className="flex mt-[60px] min-h-screen">
+                <div className="w-[300px] px-4 py-6 border-r-2">
+                    <SideMenu/>
+                </div>
+                <div className="w-full py-6 px-4">
+                    {children}
+                </div>
+            </div>
+        </>
+    );
+};
