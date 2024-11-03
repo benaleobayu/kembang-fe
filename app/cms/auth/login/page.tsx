d2c5eec1b0722fa80a9fd423722b3a7c8c4223e2
benaleobayu <benaleobs@gmail.com>
2024-11-03 21:15:29 +0700

    ADD : login page

--- app/cms/auth/login/page.tsx
+++ app/cms/auth/login/page.tsx
@@ -1,9 +1,10 @@
 // @flow
 import * as React from 'react';
+import FormLogin from "@/app/cms/auth/login/form_login";
 
 type Props = {};
 export default function Page(props: Props) {
-    const myLogo = "assets/logo/logo.png"
+    const myLogo = process.env.PATH_LOGO
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-purple-300">
@@ -16,6 +17,7 @@ export default function Page(props: Props) {
                             <p className="text-4xl font-thin text-purple-500">Hello</p>
                             <p className="text-4xl font-bold text-purple-500">Welcome</p>
                         </div>
+                        <FormLogin/>
                     </div>
                 </div>
                 <div className="bg-blue-200">
