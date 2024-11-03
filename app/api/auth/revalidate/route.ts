af41ce013d73b9d186987607b90b33a90a0fb1a3
benaleobayu <benaleobs@gmail.com>
2024-11-03 21:15:58 +0700

    ADD : API revalidate token

---
+++ app/api/auth/revalidate/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const cookieName: any = 'accessToken';
+    const cookie = req.cookies.get(cookieName);
+    const token = cookie ? cookie.value : null; // Get the token value
+
+    const apiUrl = `${process.env.AUTH_URL}/auth/revalidate-token?token=${token}`;
+
+    // Log the token for debugging (remove in production)
+    console.log("Token:", token);
+    console.log("url:", apiUrl);
+
+    try {
+        if (!token) {
+            return NextResponse.json({
+                status: 401,
+                message: 'Unauthorized: No token provided',
+            });
+        }
+
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('REVALIDATE FAILED');
+        }
+
+        const data = await response.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("Error:", error); // Log the error for debugging
+        return NextResponse.json({
+            status: 500,
+            message: error.message,
+        });
+    }
+}
