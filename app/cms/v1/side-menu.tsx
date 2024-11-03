b2471f20bc00ee33b16f931ad31be30dc4e52c1b
benaleobayu <benaleobs@gmail.com>
2024-11-03 23:43:19 +0700

    ADD : dynamic side menus

--- app/cms/v1/side-menu.tsx
+++ app/cms/v1/side-menu.tsx
@@ -1,11 +1,4 @@
-import {
-    Calculator,
-    Calendar,
-    CreditCard,
-    Settings,
-    Smile,
-    User,
-} from "lucide-react"
+"use client";
 
 import {
     Command,
@@ -15,48 +8,80 @@ import {
     CommandItem,
     CommandList,
     CommandSeparator,
-    CommandShortcut,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
+import Link from "next/link";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+import {
+    LuBoxes,
+    LuFlagTriangleRight,
+    LuIndent,
+    LuLayoutDashboard, LuLock,
+    LuMap,
+    LuMapPin,
+    LuUserCog,
+    LuUsers
+} from "react-icons/lu";
+
+const iconMap = {
+    LuBoxes: LuBoxes,
+    LuFlagTriangleRight: LuFlagTriangleRight,
+    LuIndent: LuIndent,
+    LuLayoutDashboard: LuLayoutDashboard,
+    LuLock: LuLock,
+    LuMap: LuMap,
+    LuMapPin: LuMapPin,
+    LuUserCog: LuUserCog,
+    LuUsers: LuUsers
+}
 
 export function SideMenu() {
+    const [menus, setMenus] = useState([]); // Initialize state with useState
+
+    useEffect(() => {
+        const fetchMenus = async () => {
+            try {
+                const response = await axios.get('/api/util/menu'); // Make sure to include the leading slash
+                setMenus(response.data.data);
+            } catch (error) {
+                console.error(error);
+                toast.error('Failed to fetch menus');
+            }
+        };
+
+        fetchMenus(); // Call the async function
+    }, []); // Empty dependency array to run once on mount
+    console.log(menus)
+
     return (
-        <Command className="rounded-lg border shadow-md w-full">
+        <Command className="rounded-lg border shadow-md w-full min-h-screen">
             <CommandInput placeholder="Type a command or search..." />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup heading="Suggestions">
-                    <CommandItem>
-                        <Calendar />
-                        <span>Calendar</span>
-                    </CommandItem>
-                    <CommandItem>
-                        <Smile />
-                        <span>Search Emoji</span>
-                    </CommandItem>
-                    <CommandItem disabled>
-                        <Calculator />
-                        <span>Calculator</span>
-                    </CommandItem>
-                </CommandGroup>
                 <CommandSeparator />
-                <CommandGroup heading="Settings">
-                    <CommandItem>
-                        <User />
-                        <span>Profile</span>
-                        <CommandShortcut>⌘P</CommandShortcut>
-                    </CommandItem>
-                    <CommandItem>
-                        <CreditCard />
-                        <span>Billing</span>
-                        <CommandShortcut>⌘B</CommandShortcut>
-                    </CommandItem>
-                    <CommandItem>
-                        <Settings />
-                        <span>Settings</span>
-                        <CommandShortcut>⌘S</CommandShortcut>
-                    </CommandItem>
-                </CommandGroup>
+                {menus.length > 0 && menus.map((group) => (
+                    <CommandGroup key={group.id} heading={group.name}>
+                        {group.menus.length > 0 ? (
+                            group.menus.map((menu) => {
+                                const IconComponent = iconMap[menu.icon];
+                                return (
+                                <Link key={menu.id} href={menu.link} passHref>
+                                    <CommandItem>
+                                        {IconComponent && <IconComponent/>} {/* Render the icon */}
+                                        <span>{menu.name}</span>
+                                    </CommandItem>
+                                </Link>
+                            )})
+                        ) : (
+                            <CommandItem>
+                                <span>No Menus Available</span>
+                            </CommandItem>
+                        )}
+                    </CommandGroup>
+                ))}
             </CommandList>
         </Command>
-    )
+    );
 }
