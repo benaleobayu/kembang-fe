fa91b1e650a1313d76321ac70550bafd91893f11
benaleobayu <benaleobs@gmail.com>
2024-11-03 23:43:06 +0700

    ADD : side menu routes

---
+++ app/api/util/menu/route.ts
@@ -0,0 +1,93 @@
+import {NextResponse} from "next/server";
+import {
+    LuBoxes,
+    LuFlagTriangleRight,
+    LuIndent,
+    LuLayoutDashboard, LuLock,
+    LuMap,
+    LuMapPin,
+    LuUserCog,
+    LuUsers
+} from "react-icons/lu";
+
+const data = [
+    {
+        id: 1,
+        name: 'Main',
+        menus: [
+            {
+                id: 1,
+                name: 'Dashboard',
+                icon: 'LuLayoutDashboard',
+                link: '/cms/v1/dashboard'
+            },
+            {
+                id: 2,
+                name: 'Customers',
+                icon: 'LuUsers',
+                link: '/cms/v1/customers'
+            },
+            {
+                id: 3,
+                name: 'Orders',
+                icon: 'LuIndent',
+                link: '/cms/v1/orders'
+            },
+            {
+                id: 4,
+                name: 'Route',
+                icon: 'LuMap',
+                link: '/cms/v1/routes'
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Masterdata',
+        menus: [
+            {
+                id: 1,
+                name: 'Products',
+                icon: 'LuBoxes',
+                link: '/cms/v1/ms/products'
+            },
+            {
+                id: 2,
+                name: 'Locations',
+                icon: 'LuMapPin',
+                link: '/cms/v1/ms/locations'
+            },
+            {
+                id: 3,
+                name: 'Brand',
+                icon: 'LuFlagTriangleRight',
+                link: '/cms/v1/ms/brands'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Setting',
+        menus: [
+            {
+                id: 1,
+                name: 'Admin',
+                icon: 'LuUserCog',
+                link: '/cms/v1/am/admins'
+            },
+            {
+                id: 2,
+                name: 'Privilege',
+                icon: 'LuLock',
+                link: '/cms/v1/am/privileges'
+            }
+        ]
+    },
+
+
+]
+
+export async function GET() {
+
+    return NextResponse.json({status: 200, message: 'success', data: data})
+}
