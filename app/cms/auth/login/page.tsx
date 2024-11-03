4c44963d2ffb128ab2e5f4f5b28c0164bb21bb3f
benaleobayu <benaleobs@gmail.com>
2024-11-03 13:37:17 +0700

    ADD : some file index init cms

---
+++ app/cms/auth/login/page.tsx
@@ -0,0 +1,32 @@
+// @flow
+import * as React from 'react';
+
+type Props = {};
+export default function Page(props: Props) {
+    const myLogo = "assets/logo/logo.png"
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-purple-300">
+            <div className="md:w-[80vw] lg:w-[70vw] h-[80vh] grid grid-cols-2">
+
+                <div className="px-12 py-16 bg-white flex justify-center items-center">
+                    <div className=" w-full border">
+                        <img src={myLogo} alt="my logo" className=""/>
+                        <div className="flex flex-col">
+                            <p className="text-4xl font-thin text-purple-500">Hello</p>
+                            <p className="text-4xl font-bold text-purple-500">Welcome</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-blue-200">
+                    <img
+                        src="/assets/images/login.png"
+                        alt="Login"
+                        className="object-cover w-full h-full"
+                    />
+                </div>
+            </div>
+
+        </div>
+    );
+};
