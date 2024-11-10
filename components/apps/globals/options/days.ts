d0d7c88cbcc0c618c7883aa2ec7df0da2af7d524
benaleobayu <benaleobs@gmail.com>
2024-11-10 22:27:06 +0700

    ADD : harcode test select options

---
+++ components/apps/globals/options/days.ts
@@ -0,0 +1,10 @@
+export const daysOptions: Array<{ value: string, label: string }> = [
+    {value: "nothing", label: "No Subscription",},
+    {value: "monday", label: "Monday",},
+    {value: "tuesday", label: "Tuesday",},
+    {value: "wednesday", label: "Wednesday",},
+    {value: "thursday", label: "Thursday",},
+    {value: "friday", label: "Friday",},
+    {value: "saturday", label: "Saturday",},
+    {value: "sunday", label: "Sunday",},
+]
---
+++ components/apps/globals/options/location.ts
@@ -0,0 +1,10 @@
+export const locationOptions: Array<{ value: number, label: string }> = [
+    { value: 1, label: 'Jakarta Utara' },
+    { value: 2, label: 'Jakarta Pusat' },
+    { value: 3, label: 'Jakarta Selatan' },
+    { value: 4, label: 'Jakarta Timur' },
+    { value: 5, label: 'Jakarta Barat' },
+    { value: 6, label: 'Tangerang' },
+    { value: 7, label: 'Tangerang Selatan' },
+    { value: 8, label: 'Bekasi' },
+]
