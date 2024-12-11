326dc8ba551ba1117fb7018bacf605dcacb4efdb
benaleobayu <benaleobs@gmail.com>
2024-12-11 23:38:38 +0700

    UPDATE : days option

--- components/apps/globals/options/days.ts
+++ components/apps/globals/options/days.ts
@@ -1,5 +1,4 @@
 export const daysOptions: Array<{ value: string, label: string }> = [
-    {value: "nothing", label: "No Subscription",},
     {value: "monday", label: "Monday",},
     {value: "tuesday", label: "Tuesday",},
     {value: "wednesday", label: "Wednesday",},
